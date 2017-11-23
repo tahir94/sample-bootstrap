1d8e1d9fedd472ce940b4dc8342946b80fa74201
tahir94 <tahirkhan.on@gmail.com>
2017-11-23 19:46:30 +0500

    working on profile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -36,24 +36,7 @@ export class HomeComponent implements OnInit {
 
 
 		// **************************************************
-		$(document).ready(function() {
-			var sidebarMainMenu = $('#sidebar-menu .main-menu');
-			var staticContent = $('#static-content');
-			staticContent.find('h1').each(function() {
-				sidebarMainMenu.append('<li id="'+ $(this).attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
-				this.title = sidebarMainMenu.find('#' + $(this).attr('id'));
-			});
-			staticContent.find('h2').each(function() {
-				this.prevTitle = sidebarMainMenu.find('#' + $(this).prevAll('h1').first().attr('id') + '-menu');
-				this.prevTitle.not(":has(ul)").append('<ul class="sub-menu"></ul>');
-				this.prevTitle.find('.sub-menu').append('<li id="'+ $(this).attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
-			});
-			sidebarMainMenu.affix({
-				  offset: {
-					top: 0  // To Modify according to the height offset
-				  }
-			});
-		});
+		
 	}
 
 	// 	$(document).ready(function() {
